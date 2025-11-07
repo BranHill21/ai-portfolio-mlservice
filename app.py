6d3410b0f4336b14751705fb28c65845ee834285
BranHill21 <brandon.hill2@verizon.net>
2025-11-07 13:34:23 -0600

    simplifying route to test with postman

--- app.py
+++ app.py
@@ -4,20 +4,24 @@ from textblob import TextBlob
 
 app = Flask(__name__)
 
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     data = request.json
+#     symbol = data.get("symbol")
+#     info = yf.Ticker(symbol).history(period="1mo")["Close"]
+#     change = (info[-1] - info[0]) / info[0]
+#     sentiment = TextBlob(symbol).sentiment.polarity
+#     recommendation = "BUY" if change > 0 and sentiment >= 0 else "SELL"
+#     return jsonify({
+#         "symbol": symbol,
+#         "price_change": round(change*100, 2),
+#         "sentiment": sentiment,
+#         "recommendation": recommendation
+#     })
+
 @app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json
-    symbol = data.get("symbol")
-    info = yf.Ticker(symbol).history(period="1mo")["Close"]
-    change = (info[-1] - info[0]) / info[0]
-    sentiment = TextBlob(symbol).sentiment.polarity
-    recommendation = "BUY" if change > 0 and sentiment >= 0 else "SELL"
-    return jsonify({
-        "symbol": symbol,
-        "price_change": round(change*100, 2),
-        "sentiment": sentiment,
-        "recommendation": recommendation
-    })
+def test_predict():
+    return jsonify({"status": "success"})
 
 if __name__ == "__main__":
     app.run(debug=True)
